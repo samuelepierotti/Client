2caa1ccd2c18e1f7e1fcac91c8223d96bbb72580
samuelepierotti <samy.pierotti@gmail.com>
2025-02-13 11:42:40 +0100

    Modifica classe Main.java del client

--- src/tcpcommunication_client/Main.java
+++ src/tcpcommunication_client/Main.java
@@ -4,19 +4,31 @@
  */
 package tcpcommunication_client;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
 /**
  *
  * @author Amministratore
  */
-public class Main {
 
+public class Main {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        InputStream is;
+        Scanner streamIn = null;
+        OutputStream os;
+        PrintWriter streamOut = null;
+        String messaggioIn, messaggioOut;
+        
         Client c = new Client("localhost");
         c.connetti("localhost",2000);
         c.chiudi();
+        
+        
     }
     
 }
