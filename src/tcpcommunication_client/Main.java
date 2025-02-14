3b14d29b3949854c001510d3d5dce799a276c8ce
samuelepierotti <samy.pierotti@gmail.com>
2025-02-14 11:44:00 +0100

    Aggiunta metodi per lettura e scrittura in Main.java del client

--- src/tcpcommunication_client/Main.java
+++ src/tcpcommunication_client/Main.java
@@ -4,9 +4,12 @@
  */
 package tcpcommunication_client;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Scanner;
+import java.net.Socket;
 
 /**
  *
@@ -17,17 +20,33 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args, Socket socket) {
         InputStream is;
         Scanner streamIn = null;
         OutputStream os;
         PrintWriter streamOut = null;
         String messaggioIn, messaggioOut;
-        
-        Client c = new Client("localhost");
-        c.connetti("localhost",2000);
+
+        try {
+            Client c = new Client("localhost");
+            c.connetti("localhost",2000);
+            
+            os = socket.getOutputStream();
+            streamOut = new PrintWriter(os);
+            streamOut.flush();
+            is = socket.getInputStream();
+            streamIn = new Scanner(is);
+            messaggioIn = streamIn.nextLine();
+            System.out.println("Messaggio del server: " + messaggioIn);
+            messaggioOut = "Ci sono!";
+            streamOut.println(messaggioOut);
+            streamOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         c.chiudi();
-        
         
     }
     
