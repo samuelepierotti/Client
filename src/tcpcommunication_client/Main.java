4e1c7564ac2cd20052922f80814254d2e28e0406
samuelepierotti <samy.pierotti@gmail.com>
2025-02-27 11:40:48 +0100

    Modifica classe Main.java del client

--- src/tcpcommunication_client/Main.java
+++ src/tcpcommunication_client/Main.java
@@ -20,7 +20,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args, Socket socket) {
+    public static void main(String[] args) {
         Client c = new Client("localhost");
         c.connetti("localhost",2000);
         c.leggi();
